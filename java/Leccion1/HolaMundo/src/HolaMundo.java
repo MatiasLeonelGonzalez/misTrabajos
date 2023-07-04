3ed8d282d83fe8a8413a5b6875828399a0803068
Matias Gonzalez <Skrowyy@gmail.com>
2023-07-04 02:28:50 -0300

    commit de visual

---
+++ java/Leccion1/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,106 @@
+
+import java.util.Scanner;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        /*System.out.println("Hola Mundo desde java");
+        
+        int miVariable = 10;
+        System.out.println(miVariable);
+        miVariable = 5;
+        System.out.println(miVariable);
+        
+        //Tipo String
+        
+        String miVariableCadena = "Bienvenidos";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "sigamos creciendo en programacion";
+        System.out.println(miVariableCadena);
+         */
+
+        //Var - inferencia de tipo en java
+        /* var miVariableEntera2 = 10;
+        var miVariableCadena2 = "seguimos estudiando";
+        System.out.println("miVariableEntera2 = " + miVariableEntera2);
+        System.out.println("miVariableCadena2 = " + miVariableCadena2);
+        //sout + tab 
+        //para ejecutar shif f6
+        //regrlas para definir una variab le ne java
+
+        var usuario = "Matias";
+        var titulo = "programador";
+        var union = titulo + " " + usuario;
+        System.out.println("union = " + union);
+         */
+        /*var a = 8;
+        var b = 4;
+        System.out.println(a + b);
+        //Ejercicio: caracteres especiales con java
+
+        var nombre = "Natalia";
+        System.out.println("nueva linea: \n" + nombre); //diagonal inversa y letra n
+        System.out.println("tabulador: \t" + nombre); //diagonal inversa y letra t (espacio para centrar)
+        System.out.println("\t\t: MENU:.");
+        System.out.println("Retroseso: \b\b"+ nombre); //caracter de retroseso
+        System.out.println("Comillas simples: \'"+nombre+"\'"); //comillas simples
+        System.out.println("Comillas dobles: \""+nombre+"\""); //comillas dobles
+        */
+        //clase Scanner
+        /*Scanner entrada = new Scanner(System.in);
+        System.out.println("digite su nombre: ");
+        var usuario2 = entrada.nextLine();
+        System.out.println("usuario2 = " + usuario2);
+        System.out.println("Escriba el titulo: ");
+        var titulo2 = entrada.nextLine();
+        System.out.println("Resultado: "+titulo2+" "+usuario2);
+        */
+        //clase 4 3.2
+        
+        /*byte numEnteroByte = 127;
+        System.out.println("numEnteroByte = " + numEnteroByte);
+        System.out.println("valor minimo del byte: "+ Byte.MIN_VALUE);
+        System.out.println("valor maximo del byte: "+ Byte.MAX_VALUE);
+        
+        short numEnteroShort = 32767;
+        System.out.println("numEnteroShort = " + numEnteroShort);
+        System.out.println("valor minimo del short: "+Short.MIN_VALUE);
+        System.out.println("valor maximo del short: "+Short.MAX_VALUE);
+        
+        int numEnteroInt = 10;
+        System.out.println("numEnteroInt = " + numEnteroInt);
+        System.out.println("valor minimo del int: "+Integer.MIN_VALUE);
+        System.out.println("valor maximo del int: "+Integer.MAX_VALUE);
+        
+        long numEnteroLong = 10;
+        System.out.println("numEnteroLong = " + numEnteroLong);
+        System.out.println("Valor minimo del long: "+Long.MIN_VALUE);
+        System.out.println("Valor maximo del long: "+Long.MAX_VALUE);
+        */
+        
+        // CLASE 4 3.3
+        
+        float numFloat = 3.4028235E38F;
+        System.out.println("numFloat = " + numFloat);
+        System.out.println("El valor minimo del float: "+Float.MIN_VALUE);
+        System.out.println("El valor maximo del float: "+Float.MAX_VALUE);
+        
+        double numDouble = 1.7976931348623157E308D;
+        System.out.println("numDouble = " + numDouble);
+        System.out.println("El valor minimo del Double: "+Double.MIN_VALUE);
+        System.out.println("El valor maximo del Double: "+Double.MAX_VALUE);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      
+        
+
+    }
+}
